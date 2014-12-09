df56bcc624abbcc7e6e33e4feae0727165221ce6
abc <abc>
2014-12-08 17:35:22 -0800

    init

---
+++ build-all-arch.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+#
+# http://wiki.openssl.org/index.php/Android
+#
+set -e
+rm -rf prebuilt
+mkdir prebuilt
+
+archs=(armeabi arm64-v8a mips mips64 x86 x86_64)
+
+for arch in ${archs[@]}; do
+    xLIB="/lib"
+    case ${arch} in
+        "armeabi")
+            _ANDROID_TARGET_SELECT=arch-arm
+            _ANDROID_ARCH=arch-arm
+            _ANDROID_EABI=arm-linux-androideabi-4.9
+            configure_platform="android-armv7" ;;
+        "arm64-v8a")
+            _ANDROID_TARGET_SELECT=arch-arm64-v8a
+            _ANDROID_ARCH=arch-arm64
+            _ANDROID_EABI=aarch64-linux-android-4.9
+            #no xLIB="/lib64"
+            configure_platform="linux-generic64 -DB_ENDIAN" ;;
+        "mips")
+            _ANDROID_TARGET_SELECT=arch-mips
+            _ANDROID_ARCH=arch-mips
+            _ANDROID_EABI=mipsel-linux-android-4.9
+            configure_platform="android -DB_ENDIAN" ;;
+        "mips64")
+            _ANDROID_TARGET_SELECT=arch-mips64
+            _ANDROID_ARCH=arch-mips64
+            _ANDROID_EABI=mips64el-linux-android-4.9
+            xLIB="/lib64"
+            configure_platform="linux-generic64 -DB_ENDIAN" ;;
+        "x86")
+            _ANDROID_TARGET_SELECT=arch-x86
+            _ANDROID_ARCH=arch-x86
+            _ANDROID_EABI=x86-4.9
+            configure_platform="android-x86" ;;
+        "x86_64")
+            _ANDROID_TARGET_SELECT=arch-x86_64
+            _ANDROID_ARCH=arch-x86_64
+            _ANDROID_EABI=x86_64-4.9
+            xLIB="/lib64"
+            configure_platform="linux-generic64" ;;
+        *)
+            configure_platform="linux-elf" ;;
+    esac
+
+    mkdir prebuilt/${arch}
+
+    . ./setenv-android-mod.sh
+
+    echo "CROSS COMPILE ENV : $CROSS_COMPILE"
+    cd openssl-1.0.1j
+
+    #export CFLAGS="-I${ANDROID_NDK_ROOT}/platforms/android-19/arch-$arch/usr/include"
+    #export LDFLAGS="-L${ANDROID_NDK_ROOT}/platforms/android-19/arch-$arch/usr/lib"
+    xCFLAGS="-fPIC -DOPENSSL_PIC -DDSO_DLFCN -DHAVE_DLFCN_H -mandroid -I$ANDROID_DEV/include -B$ANDROID_DEV/$xLIB -O3 -fomit-frame-pointer -Wall"
+
+    perl -pi -e 's/install: all install_docs install_sw/install: install_docs install_sw/g' Makefile.org
+    ./Configure shared -no-threads -no-asm -no-zlib -no-ssl2 -no-ssl3 -no-comp -no-hw -no-engine --openssldir=/usr/local/ssl/android-19/ $configure_platform $xCFLAGS
+
+    make clean
+    make depend
+    make all
+
+    file libcrypto.so.1.0.0
+    file libssl.so.1.0.0
+    cp libcrypto.so.1.0.0 ../prebuilt/${arch}/libcrypto.so
+    cp libssl.so.1.0.0 ../prebuilt/${arch}/libssl.so
+    cd ..
+done
+exit 0
+
